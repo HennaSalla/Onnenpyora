a4498a63712151693b4d407ecf289bd71bc3619f
HennaSalla <51696@edu.raseko.fi>
2025-04-29 13:49:04 +0300

    29.4.2025 lisätty alustava ulkonäkö otsikolle

--- onnenpyora/src/Otsikko.jsx
+++ onnenpyora/src/Otsikko.jsx
@@ -1,1 +1,17 @@
 // Tämä tiedosto tekee koko sovelluksen otsikon.
+
+// lisätään tarvittavat importit
+
+import {FC} from 'react';
+import styled from 'styled-components';
+
+const OtsikkoSisältö = styled.header`
+    background-color: #191970;
+    color: #F0F8FF;
+    min-height: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 50px;
+`;
