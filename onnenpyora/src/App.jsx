bf8a8928931083d0bd08211ae82213a0d9785007
HennaSalla <51696@edu.raseko.fi>
2025-04-29 12:12:35 +0300

    29.4.2025 luotu päätiedosto projektille

--- onnenpyora/src/App.jsx
+++ onnenpyora/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Päätiedosto jossa yhdistetään muut tiedostot ja halitaan osallistujien lisäämisen, poistamisen, lajittelun ja sekoituksen
 
+// Tarvittavat importit
+import styled from 'styled-components';
+
+// Tuodaan loput tiedostot pää tiedostoon
+import {Osallistujat} from './Osallistujat';
+import {Kysymys} from './Kysymys';
+import {Pyora} from './Pyora';
+import {Otsikko} from './Otsikko';
+
+// Viten mukana tulleiden tiedostojen importit
+import './App.css';
+import {useState} from 'react'
+
+// Sivun omat ulkonäkö muutokset
+const Main = styled.main`
+  display: flex;
+  justify-content: space-around;
+  padding: 20px;
+  background-color: #050513;
+
+  @media (max-width: 768px) {
+    flex-direction: column-reverse;
+    align-items: center;
+  }
+`;
+
+// Ohjelman päätiedoston luonti
+export const MAX_PARTICIPANTS = 100; // Laittaa osallistuja rajan 100
+
+// Halitaan ossalistujien nimien lisäämistä, poistamista, lajittelua ja sekoitusta
 function App() {
-  const [count, setCount] = useState(0)
+  const [names, setNames] = useState<string[]>([]);
 
+  // Osallistujien lisäämisen hallinta
+  const lisääOsallistuja = (name: string) => {
+    if (names.length < MAX_PARTICIPANTS) {
+      setNames([...names, name]);
+    }
+  };
+
+  // Osallistujien poiston hallinta
+  const poistaOsallistuja = (index: number) => => {
+    setNames(names.filter((_, i) => i !== index));
+  };
+
+  // Osallistujien sekoitus randomisti
+  const sekoitaOsallistujat = () => {
+    const shuffledNames = [...names].sort(() => Math.random() - 0.5);
+    setNames(shuffledNames);
+  };
+
+  // Osallistujien lajittelu aakkos järjestykseen
+  const lajitteleOsallistujat = () => {
+    const sortedNames = [...names].sort((a,b) => a.localeCompare(b));
+    setNames(sortedNames);
+  };
+
+  // Tuodaan muut komponentit tiedostoon
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Otsikko />
+      <Kysymys />
+      <Main>
+        <Osallistujat
+          handleAddName={lisääOsallistuja}
+          handleRemoveName={poistaOsallistuja}
+          shuffleNames={sekoitaOsallistujat}
+          sortNames={lajitteleOsallistujat}
+          names={names}
+        />
+        <Pyora participants={names} />
+      </Main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
